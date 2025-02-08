622a27129bde516e2233c6fc8927cae1673cdcac
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2025-02-08 13:43:02 -0500

    add application struct and start server

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 )
 
+type config struct {
+	port int
+}
+
+func NewConfig() *config {
+	return &config{
+		port: 8080,
+	}
+}
+
+type application struct {
+	config config
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", app.config.port),
+	}
+	log.Println("Starting Server", "addr", srv.Addr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-    log.Println("Hello Budget-Track")
+	config := NewConfig()
+
+	app := &application{
+		config: *config,
+	}
+
+	err := app.serve()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
