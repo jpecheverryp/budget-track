005607c8d1efcf7c7637a31dedb9f193f442376a
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2025-02-09 22:40:17 -0500

    index route redirect to login

--- routes.go
+++ routes.go
@@ -7,6 +7,7 @@ import (
 func (app *application) routes() http.Handler {
     mux := http.NewServeMux()
 
+    mux.HandleFunc("GET /", app.handler.Index)
     mux.HandleFunc("GET /login", app.handler.Login)
 
     return mux
