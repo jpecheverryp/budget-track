a1b481668a2ac02e4db3a3847fb9676761f276ee
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2025-02-11 23:07:23 -0500

    add standard loggin

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,27 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	port := flag.Int("port", 8080, "HTTP Network Port")
-    flag.Parse()
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	mux := http.NewServeMux()
 
-    fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", getIndex)
-    mux.HandleFunc("GET /transactions/view/{id}", getTransactionView)
-    mux.HandleFunc("GET /transactions/create", getTransactionCreate)
-    mux.HandleFunc("POST /transactions/create", postTransactionCreate)
+	mux.HandleFunc("GET /transactions/view/{id}", getTransactionView)
+	mux.HandleFunc("GET /transactions/create", getTransactionCreate)
+	mux.HandleFunc("POST /transactions/create", postTransactionCreate)
 
+	logger.Info("starting server", "port", *port)
 
-	log.Printf("starting server on :%d", *port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
