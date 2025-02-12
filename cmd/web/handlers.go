7ce30a29adf81eab973a72a158dd13e5e4e33108
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2025-02-11 22:07:37 -0500

    add base layout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,19 +9,24 @@ import (
 )
 
 func getIndex(w http.ResponseWriter, r *http.Request) {
-    ts, err := template.ParseFiles("./ui/html/pages/home.html")
-    if err!=nil{
-        log.Print(err.Error())
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        return
-    }
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+	}
 
-    err = ts.Execute(w, nil)
-    if err!=nil{
-        log.Print(err.Error())
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        return
-    }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func getTransactionView(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +35,7 @@ func getTransactionView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-    fmt.Fprintf(w, "Showing a single transaction info with id: %d", id)
+	fmt.Fprintf(w, "Showing a single transaction info with id: %d", id)
 }
 
 func getTransactionCreate(w http.ResponseWriter, r *http.Request) {
