b4fa5d2c0b4cd531b4e807bda7845582c9f7403b
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2025-02-26 23:15:15 -0500

    add sessions to middleware using justinas alice library

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,21 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /{$}", app.getIndex)
-	mux.HandleFunc("GET /test", app.getTest)
-	mux.HandleFunc("GET /dashboard", app.getDashboard)
-	mux.HandleFunc("GET /transactions/view/{id}", app.getTransactionView)
-	mux.HandleFunc("GET /transactions/create", app.getTransactionCreate)
+	//File server will go here, after adding static
 
-	mux.HandleFunc("POST /accounts/create", app.postAccountCreate)
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
-	mux.HandleFunc("GET /auth/register", app.getRegister)
-	mux.HandleFunc("POST /auth/register", app.postRegister)
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.getIndex))
+	mux.Handle("GET /test", dynamic.ThenFunc(app.getTest))
+	mux.Handle("GET /dashboard", dynamic.ThenFunc(app.getDashboard))
+	mux.Handle("GET /transactions/view/{id}", dynamic.ThenFunc(app.getTransactionView))
+	mux.Handle("GET /transactions/create", dynamic.ThenFunc(app.getTransactionCreate))
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	mux.Handle("POST /accounts/create", dynamic.ThenFunc(app.postAccountCreate))
 
+	mux.Handle("GET /auth/register", dynamic.ThenFunc(app.getRegister))
+	mux.Handle("POST /auth/register", dynamic.ThenFunc(app.postRegister))
+
+    standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+    return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/alexedwards/scs/postgresstore v0.0.0-20250212122300-421ef1d8611c
 	github.com/alexedwards/scs/v2 v2.8.0
 	github.com/google/uuid v1.6.0
+	github.com/justinas/alice v1.2.0
 	github.com/lib/pq v1.4.0
 )
